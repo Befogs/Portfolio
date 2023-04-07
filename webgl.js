a401dce6c1097e3e15e9f2cba76c8a3e9c264beb
Luca Rose <63530231+lucacrose@users.noreply.github.com>
2023-04-06 21:14:38 -0500

    Update webgl.js

--- webgl.js
+++ webgl.js
@@ -1,38 +1,53 @@
 const vs = `
-    attribute vec4 v_position;
-
-    void main() {
-      gl_Position = v_position;
-    }     
+  attribute vec2 coord;
+  
+  void main(void) {
+    gl_Position = vec4(coord, 0.0, 1.0);
+  }  
 `;
 
 const fs = `
-    precision mediump float;
-    
-    uniform float t; 
-
-    void main() {
-       gl_FragColor = vec4(fract(t * gl_FragCoord.xy / vec2(16., 32.)), 0, 1); 
-    }
+  uniform mediump float millisecs;
+  
+  void main(void) {
+    mediump float osc = (sin(millisecs/1000.0) + 1.0) / 2.0;
+    gl_FragColor = vec4(osc, osc, osc, 1.0);
+  }
 `;
 
 var gl = document.querySelector("canvas").getContext("webgl");
-var shader_program = twgl.createProgram(gl, [vs, fs]);
-gl.useProgram(shader_program);
-var vertexPositionAttribute = gl.getAttribLocation(shader_program, "v_position");
-var quad_vertex_buffer = gl.createBuffer();
-var quad_vertex_buffer_data = new Float32Array([ 
-    -1.0, -1.0, 0.0,
-     1.0, -1.0, 0.0,
-    -1.0,  1.0, 0.0,
-    -1.0,  1.0, 0.0,
-     1.0, -1.0, 0.0,
-     1.0,  1.0, 0.0]);
-gl.bindBuffer(gl.ARRAY_BUFFER, quad_vertex_buffer);
-gl.bufferData(gl.ARRAY_BUFFER, quad_vertex_buffer_data, gl.STATIC_DRAW);
-gl.vertexAttribPointer(vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
-gl.enableVertexAttribArray(vertexPositionAttribute)
-gl.drawArrays(gl.TRIANGLES, 0, 6);
 
-var locationOfFoo = gl.getUniformLocation(shader_program, "t");
-gl.uniform1f(locationOfFoo, .1);
+const vertShader = gl.createShader(gl.VERTEX_SHADER);
+gl.shaderSource(vertShader, vs);
+gl.compileShader(vertShader);
+
+const fragShader = gl.createShader(gl.FRAGMENT_SHADER);
+gl.shaderSource(fragShader, fs);
+gl.compileShader(fragShader);
+
+if (!gl.getShaderParameter(fragShader, gl.COMPILE_STATUS)) {
+    console.error(gl.getShaderInfoLog(fragShader));
+}
+
+const prog = gl.createProgram();
+gl.attachShader(prog, vertShader);
+gl.attachShader(prog, fragShader);
+gl.linkProgram(prog);
+gl.useProgram(prog);
+
+const vertBuf = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, vertBuf);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([-1,1,  -1,-1,  1,-1, 1,1]), gl.STATIC_DRAW);
+
+const coordPtr = gl.getAttribLocation(prog, "coord");
+gl.vertexAttribPointer(coordPtr, 2, gl.FLOAT, false, 0, 0);
+gl.enableVertexAttribArray(coordPtr);
+gl.clearColor(1,0,0,1);
+
+const millisecsPtr = gl.getUniformLocation(prog, "millisecs");
+const start = new Date().getTime();
+
+window.setInterval(function() {
+    gl.uniform1f(millisecsPtr, (new Date().getTime())-start);
+    gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
+}, 50);
